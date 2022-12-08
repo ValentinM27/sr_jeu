4d9550a1bec30f5702bb9d9fb9960d2201134fc1
Valentin Marguerie <74249732+ValentinM27@users.noreply.github.com>
2022-12-08 19:26:12 +0100

    [Player] - Add player struct

--- server/gamefiles/game.h
+++ server/gamefiles/game.h
@@ -1,25 +1,28 @@
 #ifndef H_GL_GAME
 #define H_GL_GAME
 
+// Import des libs et définition des paramètres
 #define NB_CARD 105
 
-// Définition de la structure d'une carte
+/* Définition de la structure d'une carte */
 typedef struct _CARD_ {
 	int valeur;
 	int cattleHead;
 } CARD;
 
+
+/* Définition de la structure d'un joueur */
+typedef struct _PLAYER_ {
+	char name[12];
+	CARD playerCards[NB_CARD];
+} PLAYER;
+
 // Variables du jeu
 int currentRound;
 CARD cards[NB_CARD];
 
-// Tableau de cartes des joueurs
-CARD j0[NB_CARD];
-CARD j1[NB_CARD];
-CARD j2[NB_CARD];
-CARD j3[NB_CARD];
-CARD j4[NB_CARD];
-CARD j5[NB_CARD];
+// Tableau des joueurs
+PLAYER players[6];
 
 // Déclaraction des fonctions
 void createCards();
